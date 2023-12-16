d1e9cb15c3fcc113e03d40efcd37803f6c7f09fe
Aligan18 <alisherganiev18@gmail.com>
2023-12-16 21:17:15 +0600

    i update session

--- PyrogramBot.py
+++ PyrogramBot.py
@@ -45,7 +45,7 @@ class PyrogramBot:
 
     def run_telebot(self):
         print('Atempt to run TeleBot')
-        self.bot.polling(long_polling_timeout=5)
+        self.bot.polling(long_polling_timeout=20)
 
     def run_events_listeners(self):
 
