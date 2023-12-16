6833041cf5ab2e32ff45933e4cf617adca137349
Aligan18 <alisherganiev18@gmail.com>
2023-12-16 21:30:35 +0600

    try deploy

--- backup/telebot_main.py
+++ backup/telebot_main.py
@@ -1,6 +1,6 @@
-from backup.pyrogram_bot import bot
-
-if __name__ == '__main__':
-    print('Atempt to run TeleBot')
-    bot.polling(none_stop=True)
-
+# from backup.pyrogram_bot import bot
+#
+# if __name__ == '__main__':
+#     print('Atempt to run TeleBot')
+#     bot.polling(none_stop=True)
+#
--- main.py
+++ main.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
     p1 = Process(target=run_telebot,  daemon=True)
     p2 = Process(target=run_pyrogram, daemon=True)
 
-    p2.start()
     p1.start()
+    p2.start()
     p1.join()
     p2.join()
 
